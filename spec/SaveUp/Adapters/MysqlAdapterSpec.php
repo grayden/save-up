67a53fa6f5e1ddf2d3487421b6d757928e2f4644
Grayden Smith <grayden@tech-vision.ca>
2014-09-06 18:23:45 -0400

    Add functionality to backup and restore

--- spec/SaveUp/Adapters/MysqlAdapterSpec.php
+++ spec/SaveUp/Adapters/MysqlAdapterSpec.php
@@ -13,10 +13,34 @@ class MysqlAdapterSpec extends ObjectBehavior
         $this->shouldHaveType('SaveUp\Adapters\MysqlAdapter');
     }
 
+
     function it_should_be_able_to_connect() 
     {
         $this->connect();
+        $this->shouldBeConnected();
+        $this->close();
+    }
 
-        $this->is_connected()->shouldReturn(true);
+    function it_should_be_able_to_dump_the_database()
+    {
+        $this->dump()->shouldStartWith("-- MySQL dump");
+    }
+
+    function it_should_be_able_to_restore_a_database()
+    {
+        $this->connect();
+        $this->dropDatabaseTables();
+        $this->restoreFromFile('fixtures/test_db_fixture.sql');
+        $this->shouldHaveTable('bears');
+        $this->close();
+    }
+
+    function it_should_be_able_drop_all_tables_in_the_database()
+    {
+        $this->connect();
+        $this->restoreFromFile('fixtures/test_db_fixture.sql');
+        $this->dropDatabaseTables();
+        $this->shouldNotHaveTable('bears');
+        $this->close();
     }
 }
--- src/SaveUp/Adapters/MysqlAdapter.php
+++ src/SaveUp/Adapters/MysqlAdapter.php
@@ -30,8 +30,55 @@ class MysqlAdapter
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->db);  
     }
 
-    public function is_connected()
+    public function close()
+    {
+        if ($this->isConnected())
+            mysqli_close($this->connection);
+    }
+
+    public function isConnected()
     {
         return !! $this->connection; 
+    }
+
+    public function dump()
+    {
+        return shell_exec("mysqldump -u $this->username -p$this->password $this->db");
+    }
+
+    public function hasTable($table)
+    {
+        return in_array($table, $this->tables());
+    }
+
+    public function dropDatabaseTables()
+    {
+        foreach($this->tables() as $table)
+        {
+            $this->dropTable($table); 
+        }
+    }
+
+    public function restoreFromFile($dumpFile)
+    {
+        shell_exec("mysql -u $this->username -p$this->password $this->db < $dumpFile");
+    }
+
+    private function tables()
+    {
+        $res = mysqli_query($this->connection, "SHOW TABLES");
+        $tables = [];
+
+        while($row = $res->fetch_array())
+        {
+            $tables[] = $row[0];
+        }
+
+        return $tables;
+    }
+
+    private function dropTable($table)
+    {
+        mysqli_query($this->connection, "DROP TABLE $table");
     }
 }
